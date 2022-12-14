5e9a1ef0812e5994f264966cff979b3071c1d881
ticofook <102195656+ticofook@users.noreply.github.com>
2022-12-14 14:22:57 -0300

    Create Brute-fuzz-escada.sh

---
+++ Brute-fuzz-escada.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+cat <<EOF
+
+Altera usuario e senha (usuario, admin, senha FUZZ)
+
+Troca --data-raw para --data
+
+Troca curl por ffuf –u
+
+Adicionar ao final o –fl
+
+Adiciona -w /usr/share/seclists/Passwords/2020-200_most_used_passwords.txt
+
+EOF
