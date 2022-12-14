e6212de0588fc638250550ec4dce2c67cf345853
ticofook <102195656+ticofook@users.noreply.github.com>
2022-12-14 13:16:02 -0300

    Create Recon-dns.sh

---
+++ Recon-dns.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "dnsenum -f /usr/share/seclists/Discovery/DNS/subdomains-top1million-5000.txt konstrua.com.br"
+
+dnsenum -f /usr/share/seclists/Discovery/DNS/subdomains-top1million-5000.txt konstrua.com.br
