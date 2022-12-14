9507fb55012e6b146ba4c1c2adedda373323750a
ticofook <102195656+ticofook@users.noreply.github.com>
2022-12-14 15:02:14 -0300

    Create 0-initial.sh

---
+++ 0-initial.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+
+echo "simoc" | sudo -S pip3 install git-dumper
+echo "simoc" | sudo -S apt update
+echo "simoc" | sudo -S apt install -y seclists
+echo "simoc" | sudo -S egrep -v '^#' /usr/share/seclists/Discovery/Web-Content/directory-list-2.3-small.txt > /home/simoc/directory-list-2.3-small.txt
+service ssh start
+
+ssh-keygen -t rsa -q -f "/home/simoc/.ssh/id_rsa" -N ""
+
+./ssh-copy.expect
+
+
