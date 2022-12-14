20240a90ceb8a962fa9bf2c0195b6cbea6ea0769
ticofook <102195656+ticofook@users.noreply.github.com>
2022-12-14 15:11:52 -0300

    Update 6-scan-ports-internal.sh

--- 6-scan-ports-internal.sh
+++ 6-scan-ports-internal.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 
-cd /dev/shm
+echo "cd /dev/shm"
 
 echo "resolvectl status"
 echo""
+echo "cd /dev/shm"
+echo ""
 cat <<EOF
-echo -e "443\n80\n22\n21\n3306" | while read port; do echo "[Escaneando porta $port ...]";echo 10.0.{0..1}.{0..255} | tr ' ' '\n' | xargs -P200 -I@ timeout 2 bash -c '2>- </dev/tcp/@/'$port' && echo "  [OPEN] @:"'$port;done
+echo -e "443\n80\n22\n21\n3306" | while read port; do echo "[Escaneando porta \$port ...]";echo 10.0.{0..1}.{0..255} | tr ' ' '\n' | xargs -P200 -I@ timeout 2 bash -c '2>- </dev/tcp/@/'\$port' && echo "  [OPEN] @:"'\$port;done
 EOF
